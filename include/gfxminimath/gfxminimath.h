86b288fcb2ae7952869dad864dc90aa82b1a1468
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-09-18 15:26:20 +0900

    vector class library added

--- include/gfxminimath/gfxminimath.h
+++ include/gfxminimath/gfxminimath.h
@@ -1,5 +1,15 @@
 #ifndef GFX_MINI_MATH_H
 #define GFX_MINI_MATH_H
+#define VCL_NAMESPACE vcl
+#include "vectorclass.h"
+#include "vectormath_exp.h"
+#include "vectormath_hyp.h"
+#include "vectormath_trig.h"
+#include "quaternion/quaternion.h"
+#include "vector3d/vector3d.h"
 namespace gfxminimath {
+using vec4 = vcl::Vec4f;
+using vec3 = vcl::Vec3Df;
+using quat = vcl::Quaternion1f;
 }
 #endif
--- src/dummy.cpp
+++ src/dummy.cpp
@@ -3,6 +3,8 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wreserved-identifier"
 TEST_CASE("vector") {
+  using namespace gfxminimath;
+  vec4 v(0.0f, 1.0f, 1.0f, 1.0f);
   CHECK_UNARY(true);
 }
 #pragma clang diagnostic pop

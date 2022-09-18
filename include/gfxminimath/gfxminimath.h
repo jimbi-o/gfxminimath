56e799a67a331f8c6cddaaf7abf3765e20f562b9
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-09-18 22:44:15 +0900

    matrix * vector implemented

--- include/gfxminimath/gfxminimath.h
+++ include/gfxminimath/gfxminimath.h
@@ -10,12 +10,31 @@ namespace gfxminimath {
 using vec4 = vcl::Vec4f;
 using vec3 = vcl::Vec3Df;
 using quat = vcl::Quaternion1f;
-using quat = vcl::Quaternion1f;
-using matrix = vcl::Vec16f;
-inline auto mul(const vec4& v, const matrix& m) {
-  return v;
+class matrix {
+ public:
+  matrix(const float m00, const float m01, const float m02, const float m03,
+         const float m10, const float m11, const float m12, const float m13,
+         const float m20, const float m21, const float m22, const float m23,
+         const float m30, const float m31, const float m32, const float m33)
+      : m0(m00, m10, m20, m30)
+      , m1(m01, m11, m21, m31)
+      , m2(m02, m12, m22, m32)
+      , m3(m03, m13, m23, m33) {
+  }
+  vec4 m0, m1, m2, m3;
+};
+inline auto mul(const matrix& m, const vec4& v) {
+  vec4 a = m.m0 * v;
+  vec4 b = m.m1 * v;
+  vec4 c = m.m2 * v;
+  vec4 d = m.m3 * v;
+  float x = horizontal_add(a);
+  float y = horizontal_add(b);
+  float z = horizontal_add(c);
+  float w = horizontal_add(d);
+  return vec4(x, y, z, w);
 }
-inline void GetValue(const vec4& src, float dst[4]) {
+inline void to_array(const vec4& src, float dst[4]) {
   src.store(dst);
 }
 }
--- src/dummy.cpp
+++ src/dummy.cpp
@@ -2,7 +2,7 @@
 #include <doctest/doctest.h>
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wreserved-identifier"
-TEST_CASE("vector * matrix") {
+TEST_CASE("matrix * vector") {
   using namespace gfxminimath;
   matrix m(1.0f, 2.0f, 3.0f, 4.0f,
            4.0f, 3.0f, 2.0f, 1.0f,
@@ -10,9 +10,9 @@ TEST_CASE("vector * matrix") {
            4.0f, 3.0f, -2.0f, 1.0f
            );
   vec4 v(1.0f, -2.0f, 3.0f, -4.0f);
-  auto result = mul(v, m);
+  auto result = mul(m, v);
   float result_array[4];
-  GetValue(result, result_array);
+  to_array(result, result_array);
 #pragma clang diagnostic ignored "-Wdouble-promotion"
   CHECK_EQ(result_array[0], doctest::Approx(-26.0f));
   CHECK_EQ(result_array[1], doctest::Approx(-13.0f));

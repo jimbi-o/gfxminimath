a0986633f28ea5402f06d1e24bc143d175517c90
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2022-09-18 11:19:44 +0900

    moved test files

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -1,1 +1,8 @@
 #include "gfxminimath/gfxminimath.h"
+#include <doctest/doctest.h>
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-identifier"
+TEST_CASE("vector") {
+  CHECK_UNARY(true);
+}
+#pragma clang diagnostic pop
--- tests/test_example.cpp
+++
@@ -1,8 +0,0 @@
-#include <doctest/doctest.h>
-#include "gfxminimath/gfxminimath.h"
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wreserved-identifier"
-TEST_CASE("log") {
-  CHECK_UNARY(true);
-}
-#pragma clang diagnostic pop
